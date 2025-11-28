af0d136c8e8a1ca9ed57aa45536a4a9355e020ba
HShokaku <qiu3187336@gmail.com>
2025-11-28 07:41:36 +0000

    add hit not in trainset calculation and precompute ref smiles & scaffold

--- molgenbench/metrics/hitrate.py
+++ molgenbench/metrics/hitrate.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from collections import defaultdict
 
 from rdkit import Chem
 from rdkit.Chem.Scaffolds import MurckoScaffold
@@ -8,6 +9,7 @@ from rdkit.Chem.MolStandardize import rdMolStandardize
 from rdkit import RDLogger
 RDLogger.DisableLog('rdApp.*')
 
+from sup_info.inchi_map_smiles import crossdock_trainset_duplicated_inchi_map_smiles
 from molgenbench.io.types import MoleculeRecord
 from molgenbench.metrics.base import Metric
        
@@ -51,11 +53,13 @@ class HitRediscoverMetric(Metric):
         return mol
     
     def _load_reference_maps(self, ref_active_path: str): 
-        cached_smiles_path = ref_active_path.replace('_reference_active_molecules.sdf', '_smiles.pkl')
-        cached_scaffold_path = ref_active_path.replace('_reference_active_molecules.sdf', '_scaffold.pkl')
+        cache_smiles_path = ref_active_path.replace('_reference_active_molecules.sdf', '_smiles.pkl')
+        cache_scaffold_path = ref_active_path.replace('_reference_active_molecules.sdf', '_scaffold.pkl')
+        cached_inchi_map_smiles_path = ref_active_path.replace('_reference_active_molecules.sdf', '_inchi_map_smiles.pkl')
+        cached_inchi_map_scaffold_path = ref_active_path.replace('_reference_active_molecules.sdf', '_inchi_map_scaffold.pkl')
         
         # load or create cached reference smiles map
-        if not os.path.exists(cached_smiles_path):
+        if not os.path.exists(cached_inchi_map_smiles_path):
             ref_smiles_set = set()
             ref_inchi_map_ref_smiles = {}
             suppl = Chem.SDMolSupplier(ref_active_path)
@@ -71,17 +75,22 @@ class HitRediscoverMetric(Metric):
                 # multi inchi map to one smiles
                 ref_inchi_map_ref_smiles.update({key: ref_smiles for key in ref_inchi_set})
                 
-            with open(cached_smiles_path, 'wb') as f:
+            with open(cached_inchi_map_smiles_path, 'wb') as f:
                 pickle.dump(ref_inchi_map_ref_smiles, f)
-        
-        with open(cached_smiles_path, 'rb') as f:
+                
+            if not os.path.exists(cache_smiles_path):
+                with open(cache_smiles_path, 'wb') as f:
+                    pickle.dump(ref_smiles_set, f)
+                    
+        with open(cached_inchi_map_smiles_path, 'rb') as f:
             ref_inchi_map_ref_smiles = pickle.load(f)
             
         # load or create cached reference scaffold map
-        if not os.path.exists(cached_scaffold_path):
+        if not os.path.exists(cached_inchi_map_scaffold_path):
             ref_smiles_set = set()
             ref_scaffold_set = set()
             ref_inchi_map_ref_scaffold = {}
+            ref_scaffold_map_smiles = defaultdict(set)
             suppl = Chem.SDMolSupplier(ref_active_path)
             for ref_mol in suppl:
                 if ref_mol is None:
@@ -94,20 +103,35 @@ class HitRediscoverMetric(Metric):
                 scaffold_smiles = self._getScaffold(ref_smiles)
                 if scaffold_smiles != '':
                     ref_scaffold_set.add(scaffold_smiles)
+                    ref_scaffold_map_smiles[scaffold_smiles].add(ref_smiles)
             
             for ref_scaffold in ref_scaffold_set:
                 ref_inchi_set = self._enumerate_tautomer_and_partial_chirality(ref_scaffold)
                 # multi inchi map to one smiles
                 ref_inchi_map_ref_scaffold.update({key: ref_scaffold for key in ref_inchi_set})
                 
-            with open(cached_scaffold_path, 'wb') as f:
+            with open(cached_inchi_map_scaffold_path, 'wb') as f:
                 pickle.dump(ref_inchi_map_ref_scaffold, f)
+            
+            if not os.path.exists(cache_scaffold_path):
+                with open(cache_scaffold_path, 'wb') as f:
+                    pickle.dump(ref_scaffold_map_smiles, f)
         
-        with open(cached_scaffold_path, 'rb') as f:
+        with open(cached_inchi_map_scaffold_path, 'rb') as f:
             ref_inchi_map_ref_scaffold = pickle.load(f)
     
         return ref_inchi_map_ref_smiles, ref_inchi_map_ref_scaffold
 
+    def _check_smiles_inchi_in_trainset(self, uniprot: str, smiles_inchi: str):
+        if uniprot not in crossdock_trainset_duplicated_inchi_map_smiles:
+            return False
+        return smiles_inchi in crossdock_trainset_duplicated_inchi_map_smiles[uniprot]
+    
+    def _check_scaffold_inchi_in_trainset(self, uniprot: str, scaffold_inchi: str):
+        if f"{uniprot}_scaffold" not in crossdock_trainset_duplicated_inchi_map_smiles:
+            return False
+        return scaffold_inchi in crossdock_trainset_duplicated_inchi_map_smiles[f"{uniprot}_scaffold"]
+    
     def compute(self, record: MoleculeRecord):
         """
         Check whether the molecule matches any reference actives by SMILES or
@@ -121,8 +145,10 @@ class HitRediscoverMetric(Metric):
             "scaffold_hit": None,
             "found_smiles": None,
             "found_smiles_inchi": None,
+            "found_smiles_inchi_in_trainset": None,
             "found_scaffold": None,
             "found_scaffold_inchi": None,
+            "found_scaffold_inchi_in_trainset": None,
         }
 
         mol = record.rdkit_mol
@@ -152,6 +178,7 @@ class HitRediscoverMetric(Metric):
             result["smiles_hit"] = True
             result["found_smiles_inchi"] = gen_smiles_inchi
             result["found_smiles"] = ref_inchi_map_ref_smiles[gen_smiles_inchi]
+            result["found_smiles_inchi_in_trainset"] = self._check_smiles_inchi_in_trainset(record.uniprot, gen_smiles_inchi)
         else:
             result["smiles_hit"] = False
         
@@ -159,6 +186,7 @@ class HitRediscoverMetric(Metric):
             result["scaffold_hit"] = True
             result["found_scaffold_inchi"] = gen_scaffold_inchi
             result["found_scaffold"] = ref_inchi_map_ref_scaffold[gen_scaffold_inchi]
+            result["found_scaffold_inchi_in_trainset"] = self._check_scaffold_inchi_in_trainset(record.uniprot, gen_scaffold_inchi)
         else:
             result["scaffold_hit"] = False
             
