7f4d392ba47d251cb7e8d2379fa3e7f011a79b95
HShokaku <qiu3187336@gmail.com>
2025-11-28 07:25:31 +0000

    add uniprot and series to record

--- molgenbench/io/reader.py
+++ molgenbench/io/reader.py
@@ -8,6 +8,8 @@ from molgenbench.io.types import MoleculeRecord
 
 
 def read_sdf_to_records(
+    uniprot: str,
+    series: str,
     path: Path,
     protein_path: str,
     pocket_path: str,
@@ -34,6 +36,8 @@ def read_sdf_to_records(
         record = MoleculeRecord(
             id=mol.GetProp("_Name"),
             smiles=smiles,
+            uniprot=uniprot,
+            series=series,
             rdkit_mol=mol,
             num_rotatable_bonds=num_rotatable_bonds,
             metadata={
@@ -80,52 +84,3 @@ def attach_docked_molecules(
             record.metadata["docked_mol"] = docked_dict[name]
 
     return records
-
-
-def read_uniprot_sdf_dir(root_dir: str) -> Dict[str, List[MoleculeRecord]]:
-    """
-    Traverse a root directory containing multiple UniProt subfolders.
-    Each subfolder should contain exactly one .sdf file.
-    Example structure:
-        TestSamples/
-        ├── O14757/
-            ├──Round1/
-                ├── De_novo_Results/
-                │   ├── PocketFlow_generated_molecules/
-                │   │   └── O14757_PocketFlow_generated_molecules.sdf
-                │   └── Hit_to_Lead_Results/
-                        └── Series14139
-                            └── DeleteHit2Lead(CrossDock)_Hit_to_Lead
-                                └── O14757_Series14139_DeleteHit2Lead(CrossDock)_Hit_to_Lead.sdf
-        ├── P12345/
-            ├──Round1/
-                ├── De_novo_Results/
-                    └── ...
-                ├── Hit_to_Lead_Results/
-                    └── ...
-
-    Args:
-        root_dir (str): Path to the root directory.
-
-    Returns:
-        Dict[str, List[MoleculeRecord]]: Mapping from UniProt ID to MoleculeRecord list.
-    """
-    root = Path(root_dir)
-    all_records: Dict[str, List[MoleculeRecord]] = {}
-
-    for subdir in root.iterdir():
-        if not subdir.is_dir():
-            continue
-
-        uniprot_id = subdir.name
-        
-        sdf_files = list(subdir.glob("*.sdf"))
-        if not sdf_files:
-            continue
-
-        # take the first SDF file by default
-        sdf_file = sdf_files[0]
-        records = read_sdf_to_records(sdf_file, source_name=uniprot_id)
-        all_records[uniprot_id] = records
-
-    return all_records
